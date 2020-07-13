998ac172bbb04cab5298b10dbdf073d1be01cb7a
Anonymous Maarten <anonymous.maarten@gmail.com>
2020-07-13 15:20:17 +0200

    Use PyYAML+pathlib in tarball generation

--- .ci/install.sh
+++ .ci/install.sh
@@ -4,5 +4,6 @@ set -ex
 
 if [[ "$(uname -s)" == 'Linux' ]]; then
     sudo apt-get -y update
-    sudo apt-get -y install libboost-test-dev
+    sudo apt-get -y install libboost-test-dev python3-pip python3-setuptools
 fi
+python3 -m pip install --user pyyaml
