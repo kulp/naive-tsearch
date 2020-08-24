1ce461c68d37fcc98bf809da18bf338c0db8e052
Anonymous Maarten <anonymous.maarten@gmail.com>
2020-08-24 17:10:18 +0200

    Make .tar.gz tarball reproducible by zero'ing the timestamp

--- create_source_tarball.py
+++ create_source_tarball.py
@@ -28,3 +28,11 @@ for tar_compression in tar_compressions:
             name = Path(tar_filebasename) / file.relative_to(WORKPATH)
             print("Adding {}...".format(name))
             tar.add(name=file, arcname=str(name))
+
+
+# Zero the embedded timestamp in the gzip'ed tarball
+targz = Path("naive-tsearch-{}.tar.gz".format(version))
+if targz.is_file():
+    with targz.open("r+b") as f:
+        f.seek(4, 0)
+        f.write(b"\x00\x00\x00\x00")
